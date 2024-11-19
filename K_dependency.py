2de0e2b138cd7f51ca1c9d9755c1fa770c247738
antonilis <ai.lis@student.uw.edu.pl>
2024-11-19 14:41:06 +0100

    Fixed problems after merging, added theoretical calculations to K_deoendency.

--- K_dependency.py
+++ K_dependency.py
@@ -5,25 +5,10 @@ from uncertainties import umath
 import utils as uts
 
 
-def get_molar_mass(probe):
-    if probe == 'EGly':
-        return 62.07
-
-    else:
-        return float(probe.split('G')[1])
-
-def calculate_Rg(Mmol):
-    Rg = 0.215 * Mmol ** (0.583) / 10  # result is in [nm]
-
-    return Rg
-
 def choose_peg(data, peg):
     df = data[data['crowder'] == peg]
     return df
 
-def calculate_number_of_monomers(Mmas):
-    polymer_number = (Mmas - 18.02) / (62.07 - 18.02)  # number of monomers in polymer
-    return polymer_number
 
 def fit_polynomial_to_K(data):
     names = data['crowder'].unique()
@@ -31,11 +16,10 @@ def fit_polynomial_to_K(data):
     coeffs_dict = {}
 
     for name in names:
-
         x = np.array(choose_peg(data, name)['concentration [M]'], dtype=np.float64)
         y, y_err = uts.get_float_uncertainty(choose_peg(data, name)['K'].apply(lambda x: umath.log(x)))
 
-        coefficients, cov_matrix = np.polyfit(x, y, 2, w= 1/y_err, cov=True)
+        coefficients, cov_matrix = np.polyfit(x, y, 2, w=1 / y_err, cov=True)
 
         errors = np.sqrt(np.diag(cov_matrix))
 
@@ -57,37 +41,51 @@ data = pd.read_csv('./results/K_DNA-DNA_in_crowder_solutions.csv')
 
 data.dropna(inplace=True)
 
-data['molar mass'] = data['crowder'].apply(get_molar_mass)
-
-density = 1
-
-data['concentration [M]'] = data['mass concentration'] * density / data['molar mass'] * 10
-
+#changing +- sign to the unc float objects
 for col in data.columns:
     data[col] = data[col].map(uts.convert_to_ufloat)
 
+data = pd.merge(data, pd.DataFrame(uts.crowders_properties()), left_on='crowder', right_index=True)
+
+density = 1
+
+data['concentration [M]'] = data['wt_%'] * density / data['MW_[g/mol]'] * 10
 
 coefficients = fit_polynomial_to_K(data)
-
-coefficients['molar mass'] = coefficients['crowder'].apply(get_molar_mass)
-coefficients['Rg'] = calculate_Rg(coefficients['molar mass'])
-coefficients['monomer numbers'] = calculate_number_of_monomers(coefficients['molar mass'])
-
+coefficients = pd.merge(coefficients, pd.DataFrame(uts.crowders_properties()), left_on='crowder', right_index=True)
 
 ###########  theretical calculations ##########################
 
 #
-eps = 8.8541878188 * 10**(-12) * 81 # vacuum * water relative permittivity
-Na = 6.02214076 * 10**(23) # avogadro number
-qe = 1.60217663 * 10**(-19) # elementary charge of electrion in Culomb
-kb = 1.380649 * 10**(-23) # boltzman constant
-R =  8.31446261815324 #
+eps = 8.8541878188 * 10 ** (-12) * 81  # vacuum * water relative permittivity
+Na = 6.02214076 * 10 ** (23)  # avogadro number
+qe = 1.60217663 * 10 ** (-19)  # elementary charge of electrion in Culomb
+kb = 1.380649 * 10 ** (-23)  # boltzman constant
+R = 8.31446261815324  #
 T = 298.15
 
-Rg_ssDNA = 0.55 # radius of ssDNA
+Rg_ssDNA = 0.55  # radius of ssDNA, I have chosen this arbitraly
 
-#experimental data
-c0 = 35 # concentration of Na+ in mmol which is equal to mol/m^3
-zi = 13 # charge of the ssDNA 13bp
-a = 0.0754/0.1754 * 4 + 0.0246/0.1754 # the anions part of the ionic strength HPO42- and H2PO4-
-Km = 0.14 # complexation constant of the PEG - Na complexation
+# experimental data
+c0 = 35  # concentration of Na+ in mmol which is equal to mol/m^3
+zi = 13  # charge of the ssDNA 13bp
+a = 0.0754 / 0.1754 * 4 + 0.0246 / 0.1754  # the anions part of the ionic strength HPO42- and H2PO4-
+Km = 0.14  # complexation constant of the PEG - Na complexation
+
+alpha = np.sqrt((eps * kb * T) / (2 * Na * qe ** 2))
+
+Zi = zi * qe / (4 * np.pi * eps)
+
+
+def calc_lambda():
+    licznik = Na * zi * qe * Zi * np.sqrt(c0)
+    mianownik = 3 * np.sqrt(6) * (3 * a + 2) ** (3 / 2) * np.e * alpha
+
+    return licznik / mianownik
+
+
+lambd = calc_lambda()
+
+theory_a2 = -(4 * a + 2) * lambd / (R * T) * Km ** 2
+
+theory_a1 = 2 * (5 * a + 3) * lambd /(R * T) * Km
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import numpy as np
 import uncertainties as unc
 import math
 from scipy.stats import linregress
+import pandas as pd
+
 
 # Function to convert strings with uncertainties to ufloat
 def convert_to_ufloat(value):
@@ -46,3 +48,24 @@ def logdef(x):
     a = x * 10 ** (-b)
     c = (a, b)
     return c
+
+
+# returns all properties of crowders, such as MW, No of monomers, Rg, Rh, etc.
+def crowders_properties():
+    data = {
+        'MW_[g/mol]': [62.07, 200, 400, 600, 1000, 1500, 3000, 6000, 12000, 20000, 35000],
+        'No_mono': [1, 4.131, 8.672, 13.212, 22.292, 33.643, 67.695, 135.800, 272.009, 453.620, 794.143],
+        'd_coef': [0.00094, 0.0012, 0.0013, 0.00135, 0.0014, 0.00145, 0.0015, 0.00155, 0.0016, 0.00165,
+                   0.0017]}  # ρ=ρ0+A⋅C, where C is PEG wt.% and ρ0 = 0.997 g/cm3
+    index = ["EGly", "PEG200", "PEG400", "PEG600", "PEG1000", "PEG1500", "PEG3000", "PEG6000", "PEG12000", "PEG20000",
+             "PEG35000"]
+    value = pd.DataFrame(data, index=index)
+    value['Rg_[nm]'] = 0.215 * value['MW_[g/mol]'] ** 0.583 / 10
+    value['Rg_err_[nm]'] = 0.215 * value['MW_[g/mol]'] ** 0.031 / 10
+    value['Rh_[nm]'] = 0.145 * value['MW_[g/mol]'] ** 0.571 / 10
+    value['Rh_err_[nm]'] = 0.145 * value['MW_[g/mol]'] ** 0.009 / 10
+    value['V_Rg_[nm]'] = 4 / 3 * np.pi * value['Rg_[nm]'] ** 3
+    value['V_Rg_err_[nm]'] = 4 * np.pi * value['Rg_[nm]'] ** 2 * value['Rg_err_[nm]']
+    value['η_[dm3/g]'] = 0.004 * value['MW_[g/mol]'] ** 0.8 * 1e-1
+    value['C*_[g/dm3]'] = 1 / value['η_[dm3/g]']
+    return (value)
